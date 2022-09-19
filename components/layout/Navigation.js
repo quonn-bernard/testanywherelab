bdfc144b1e8f6e30092523a416cd03ec80cbc545
Quonn S Bernard <quonn.bernard@gmail.com>
2022-09-19 17:15:03 -0400

    Adds logo to navigation(#7)

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -12,10 +12,11 @@ import {
   MenuItem,
   MenuList,
   useDisclosure,
-  Image,
+  Container,
 } from "@chakra-ui/react";
+import Image from "next/image";
 import GlobalSearch from "../GlobalSearch";
-
+import Logo from "../../public/logo.png";
 const MenuItems = ({ children }) => (
   <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
     {children}
@@ -28,119 +29,126 @@ const Navigation = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      w="100%"
-      maxW="auto"
-      p="1rem"
-      m="0"
-      borderBottom={"1px solid black"}
-    >
-      <Text fontSize={{ base: "lg", md: "2xl" }} mr={5}>
-        <Link href="/">
-          <Image w={{base: "65vw", sm: "50vw", md: "35vw", lg: "20vw" }} src="https://res.cloudinary.com/dowmtolou/image/upload/v1663343219/Test_Anywhere_ig_logo_12_dz16wb.png" />
-        </Link>
-      </Text>
-      <HStack>
-        <Show below="md">
-          <GlobalSearch />
-        </Show>
-        <Box display={{ sm: "block", md: "none" }} onClick={handleToggle}>
-          <svg
-            width="25px"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </Box>
-        <Show below="md">{/* <DarkModeSwitch /> */}</Show>
-      </HStack>
-
-      <Box
-        display={{ base: show ? "block" : "none", md: "flex" }}
-        width={{ base: "full", md: "auto" }}
-        alignItems="center"
-        flexGrow={1}
-        fontWeight={600}
+    <Container w="100%" maxW={{ base: "100%", md: "65%", lg: "80%" }}>
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        // wrap="wrap"
+        w={{ base: "100%" }}
+        maxW="auto"
+        py="1rem"
+        m="0"
+        borderBottom={"1px solid black"}
       >
-        <Menu alignItems="center" fontWeight={600} isOpen={isOpen}>
-          <MenuButton
-            borderRadius="md"
-            borderWidth="0"
-            _hover={{}}
-            onMouseEnter={onOpen}
-            onMouseLeave={onClose}
-            mr={5}
-            fontWeight="600"
-          >
-            <Link href="/services">SERVICES</Link>
-          </MenuButton>
-          <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
-            <MenuItem>
-              <Link href="/categories/routine-labs">Routine Labs</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/infectious-disease">
-                infectious Disease
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/diabetes-screening">Diabetes Screen</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/clotting-screening">Clotting Screen</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/urine-based-testing">
-                Urine Based Tests
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/job-onboarding">Job Onboarding</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/womens-health">Womens' Health</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/immunizations">Immunizations</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/categories/test-bundles">Test Bundles</Link>
-            </MenuItem>
-          </MenuList>
-        </Menu>
-        <Text mr={5}>
-          <Link href="/about">ABOUT </Link>
-        </Text>
-        <Text mr={5}>
-          <Link href="/contact">CONTACT US</Link>
-        </Text>
-      </Box>
-
-      <Box
-        display={{
-          base: show ? "block" : "none",
-          sm: show ? "block" : "none",
-          md: "block",
-        }}
-        mt={{ base: 4, md: 0 }}
-      >
-        <HStack gap={3}>
-          <Show above="md">
+        <HStack>
+          <Flex h="100%" mr={{base: 0, lg: 10}}>
+            <Link href="/" border={"1px solid white"}>
+              <Image src={Logo} />
+            </Link>
+          </Flex>
+          <Show below="md">
             <GlobalSearch />
           </Show>
-          <Button rounded={"full"} variant="link">
-            CREATE ACCOUNT
-          </Button>
-          <Show above="md">{/* <DarkModeSwitch /> */}</Show>
+          <Box display={{ sm: "block", md: "none" }} onClick={handleToggle}>
+            <svg
+              width="25px"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </Box>
+          <Show below="md">{/* <DarkModeSwitch /> */}</Show>
         </HStack>
-      </Box>
-    </Flex>
+
+        <Box
+          display={{ base: show ? "block" : "none", md: "flex" }}
+          width={{ base: "full", md: "auto" }}
+          alignItems="center"
+          flexGrow={1}
+          fontWeight={600}
+          flexDirection={"row"}
+        >
+          <Menu alignItems="center" fontWeight={600} isOpen={isOpen}>
+            <MenuButton
+              borderRadius="md"
+              borderWidth="0"
+              _hover={{}}
+              onMouseEnter={onOpen}
+              onMouseLeave={onClose}
+              mr={5}
+              fontWeight="600"
+            >
+              <Link href="/services">SERVICES</Link>
+            </MenuButton>
+            <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
+              <MenuItem>
+                <Link href="/categories/routine-labs">Routine Labs</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/infectious-disease">
+                  infectious Disease
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/diabetes-screening">
+                  Diabetes Screen
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/clotting-screening">
+                  Clotting Screen
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/urine-based-testing">
+                  Urine Based Tests
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/job-onboarding">Job Onboarding</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/womens-health">Womens' Health</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/immunizations">Immunizations</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href="/categories/test-bundles">Test Bundles</Link>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Text mr={5}>
+            <Link href="/about">ABOUT </Link>
+          </Text>
+          <Text mr={5}>
+            <Link href="/contact">CONTACT</Link>
+          </Text>
+        </Box>
+
+        <Box
+          display={{
+            base: show ? "block" : "none",
+            sm: show ? "block" : "none",
+            md: "block",
+          }}
+          mt={{ base: 4, md: 0 }}
+        >
+          <HStack gap={3}>
+            <Show above="md">
+              <GlobalSearch />
+            </Show>
+            <Button rounded={"full"} variant="link">
+              CREATE ACCOUNT
+            </Button>
+            <Show above="md">{/* <DarkModeSwitch /> */}</Show>
+          </HStack>
+        </Box>
+      </Flex>
+    </Container>
   );
 };
 
